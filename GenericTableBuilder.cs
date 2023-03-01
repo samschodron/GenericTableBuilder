50091cd397d1e918a898ed3d7468348ae91f710f
schodron@wisc.edu <schodron@wisc.edu>
2023-03-01 10:51:03 -0600

    Add project files.

---
+++ GenericTableBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class GenericTableBuilder
+{
+	public static string BuildTable<T>(string name, string column, IEnumerable<T> values)
+	{
+		var testType = values.First();
+		StringBuilder declare = new StringBuilder();
+		int counter;
+		string type = "";
+
+		if(type is bool)
+		{
+
+		}
+		else if(type is int)
+		{
+
+		}
+		else if(type is long)
+		{
+
+		}
+		else if(type is float || type is double)
+		{
+
+		}
+
+	}
+}
