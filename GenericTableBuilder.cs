ac23e151483d2ffa57e5399c14fdf34e49874b25
schodron@wisc.edu <schodron@wisc.edu>
2023-03-01 11:01:05 -0600

    Single column table completed

--- GenericTableBuilder.cs
+++ GenericTableBuilder.cs
@@ -8,26 +8,57 @@ public class GenericTableBuilder
 	public static string BuildTable<T>(string name, string column, IEnumerable<T> values)
 	{
 		var testType = values.First();
-		StringBuilder declare = new StringBuilder();
+		StringBuilder sqlString = new StringBuilder();
 		int counter;
 		string type = "";
 
-		if(type is bool)
+		if(testType is bool)
 		{
-
+			type = "BIT";
 		}
-		else if(type is int)
+		else if(testType is int)
 		{
-
-		}
-		else if(type is long)
+            type = "INT";
+        }
+		else if(testType is long)
 		{
-
-		}
-		else if(type is float || type is double)
+            type = "BIGINT";
+        }
+		else if(testType is float || testType is double)
 		{
-
+            type = "DECIMAL";
+        }
+		else if(testType is string)
+		{
+			// When variable type is string we want to surround value in single quotes.
+			var columnMax = values.Cast<string>().Aggregate((max, cur) => max.Length > cur.Length ? max : cur);
+			sqlString.Append($"DECLARE @@{name} TABLE({column} VARCHAR({columnMax.Length}));");\
+			counter = values.Count() - 1;
+			foreach(var value in values)
+			{
+				if(counter == columnValues.Count() -1 || counter % 1000 == 999)
+				{
+					sqlString.Append($"INSERT INTO @@{name} {column} VALUES ");
+				}
+                sqlString.Append($"('{value}'){(counter % 1000 == 0 ? ";" : ",")}");
+				counter--;
+            }
+		}
+		else
+		{
+			throw new ArgumentException($"Variable type is not currently supported: {testType}")
 		}
 
-	}
+		sqlString.Append($"DECLARE @@{name} TABLE({column} {type});");
+		counter = values.Count() - 1;
+        foreach (var value in values)
+        {
+            if (counter == columnValues.Count() - 1 || counter % 1000 == 999)
+            {
+                sqlString.Append($"INSERT INTO @@{name} {column} VALUES ");
+            }
+            sqlString.Append($"({value}){(counter % 1000 == 0 ? ";" : ",")}");
+            counter--;
+        }
+    }
 }
